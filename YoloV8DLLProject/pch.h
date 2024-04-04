a447b0049e2aa305588e9c7fb792a96a84477f38
statking <60002940+kimwoonggon@users.noreply.github.com>
2024-04-04 16:28:20 +0900

    Add project files.

---
+++ YoloV8DLLProject/pch.h
@@ -0,0 +1,19 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+// You don't need "framework.h"
+// #include "framework.h"
+#include <ATen/ATen.h>
+#include <time.h>
+#include <torch/script.h>
+#include <torch/torch.h>
+#include <algorithm>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#endif
