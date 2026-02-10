75863d6a46a390ffb55132de53f465fe13c22564
statking <60002940+kimwoonggon@users.noreply.github.com>
2026-02-10 21:47:06 +0900

    delete old dll init

--- YoloV8DLLProject/dllmain.cpp
+++ YoloV8DLLProject/dllmain.cpp
@@ -66,20 +66,5 @@ extern "C" {
         }
     }
 
-    // Legacy functions or other initialization if required
-    BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-    {
-        switch (ul_reason_for_call)
-        {
-        case DLL_PROCESS_ATTACH:
-        case DLL_THREAD_ATTACH:
-        case DLL_THREAD_DETACH:
-        case DLL_PROCESS_DETACH:
-            break;
-        }
-        return TRUE;
-    }
+    
 }
